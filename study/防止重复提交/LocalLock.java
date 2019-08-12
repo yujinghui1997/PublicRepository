bc1dd9c66902958ac05f019d6839792eb52b747b
yujinghui1997 <1553739947@qq.com>
2019-08-12 08:53:42 +0800

    aop防止重复提交

---
+++ study/防止重复提交/LocalLock.java
@@ -0,0 +1,27 @@
+package com.shixun.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface LocalLock {
+
+	 /**
+     * @author fly
+     */
+    String key() default "";
+
+    /**
+     * 过期时间 TODO 由于用的 guava 暂时就忽略这属性吧 集成 redis 需要用到
+     *
+     * @author fly
+     */
+    int expire() default 5;
+}
